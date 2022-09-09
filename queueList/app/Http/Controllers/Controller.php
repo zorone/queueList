35ad348138cf541177882bbe939238199ea40343
Imron  Jehleh <imron.jehleh@gmail.com>
2022-09-09 20:13:34 +0700

    Try to configure environments

---
+++ queueList/app/Http/Controllers/Controller.php
@@ -0,0 +1,16 @@
+use Inertia\Inertia;
+
+class EventsController extends Controller
+{
+    public function show(Event $event)
+    {
+        return Inertia::render('Event/Show', [
+            'event' => $event->only(
+                'id',
+                'title',
+                'start_date',
+                'description'
+            ),
+        ]);
+    }
+}
